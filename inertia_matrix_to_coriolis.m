47d4846f9f6c0805e1ad3dbf0d646f387913311f
MartinaDoku2001 <117894805+MartinaDoku2001@users.noreply.github.com>
2024-04-10 22:54:03 +0300

    Update inertia_matrix_to_coriolis.m

--- inertia_matrix_to_coriolis.m
+++ inertia_matrix_to_coriolis.m
@@ -1,25 +1,35 @@
-function[c, C] = inertia_matrix_to_coriolis(M)
-    % takes as inputs:
+function [c, C] = inertia_matrix_to_coriolis(M, q, dq)
+    % Takes as inputs:
     %   - M = the inertia matrix
-    %   
-    % output:
+    %   - q = a vertical vector of q values
+    %   - dq = a vertical vector dot_q
+    % Output:
     %   - c = robot centrifugal and Coriolis term
     %   - C = Christoffel matrices
 
-    syms q1 q2 q3 dq1 dq2 dq3 ddq1 ddq2 ddq3 a1 a2 a3 a4 a5 a6 real
-  
-    disp("Christoffel matrices")
-    q=[q1;q2;q3];
-    M1=M(:,1);
-    C1=(1/2)*(jacobian(M1,q)+jacobian(M1,q)'-diff(M,q1))
-    M2=M(:,2);
-    C2=(1/2)*(jacobian(M2,q)+jacobian(M2,q)'-diff(M,q2))
-    M3=M(:,3);
-    C3=(1/2)*(jacobian(M3,q)+jacobian(M3,q)'-diff(M,q3))
-    disp("robot centrifugal and Coriolis terms")
-    dq=[dq1;dq2;dq3];
-    c1=dq'*C1*dq;
-    c2=dq'*C2*dq;
-    c3=dq'*C3*dq;
-    c=[c1;c2;c3]
-    C=[C1;C2;C3]
+    % Initialize variables
+    n = length(q); % Number of joints
+    C = cell(n, 1); % Initialize cell array to store Christoffel matrices
+    
+    % Loop over each joint
+    for i = 1:n
+        % Compute Christoffel matrix for the i-th joint
+        disp(['Christoffel matrix for joint ', num2str(i)])
+        Mi = M(:, i); % Select the i-th column of the inertia matrix
+        Ci = (1/2) * (jacobian(Mi, q) + jacobian(Mi, q)' - diff(M, q(i)));
+        C{i} = Ci; % Store the Christoffel matrix
+        
+        % Display the Christoffel matrix
+        disp(['C', num2str(i), ' = ']);
+        disp(Ci);
+    end
+    
+    % Compute robot centrifugal and Coriolis terms
+    disp("Robot centrifugal and Coriolis terms")
+    c = sym(zeros(n, 1));
+    for i = 1:n
+        c(i) = dq' * C{i} * dq;
+        disp(['c', num2str(i), ' = ']);
+        disp(c(i));
+    end
+end
