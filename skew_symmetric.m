693687cb0995996616bd5718ceede99cffde1e41
elenamuia <elenamuia2001@gmail.com>
2024-04-14 12:08:46 +0200

    delete duplicate functions

--- skew_symmetric.m
+++
@@ -1,8 +0,0 @@
-function S = skew_sym(v)
-    % Check that the input vector is a column vector
-    v = v(:);
-    % Create skew_symmetric matrix 3x3
-    S = [ 0,    -v(3),  v(2);
-          v(3),  0,    -v(1);
-         -v(2),  v(1),  0 ];
-end
